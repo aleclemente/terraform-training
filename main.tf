b09f904b4e97f099f2cd4003f16aae6081a34e07
Alexandre Clemente <aleclemente@gmail.com>
2025-08-07 09:18:24 -0300

    feat: providers e gerenciamento de versão

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.5.1"
+    }
+  }
+}
 
 resource "local_file" "txtFileExample" {
   filename = "${path.module}/example.txt"
