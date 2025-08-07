54082510641fe8099c842e2e08ad2cddc2d1836f
Alexandre Clemente <aleclemente@gmail.com>
2025-08-07 09:27:25 -0300

    feat: Utilizando variáveis

--- main.tf
+++ main.tf
@@ -9,6 +9,5 @@ terraform {
 
 resource "local_file" "txtFileExample" {
   filename = "${path.module}/example.txt"
-  content  = "This is an example text file created by Terraform."
-  
+  content  = var.file_content
 }
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "file_content" {
+  description = "The content to write to the example.txt file"
+  type        = string
+  default     = "This is an example text file created by Terraform."
+}
