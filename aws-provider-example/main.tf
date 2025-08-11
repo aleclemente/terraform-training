953cf43bf7e413e993b464959d0e62db0fbc7d7b
Alexandre Clemente <aleclemente@gmail.com>
2025-08-11 18:33:23 -0300

    feat: add and setup secretsmanager data, security group, rout table for public access, user data in EC2 instance

--- aws-provider-example/main.tf
+++ aws-provider-example/main.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+//CAUTION:arn below ir just an example. It is necessary to add a secret manager credentials in System Manager on aws, get the arn and write below
+data "aws_secretsmanager_secret" "secret_example" {
+  arn = "arn:aws:secretsmanager:us-east-1:123456789012:secret:example"
+}
+
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.secret_example.id
+}
+
 provider "aws" {
   region     = "us-east-1"
   profile    = "default"
@@ -30,14 +39,44 @@ resource "aws_subnet" "subnet_example" {
   }
 }
 
+resource "aws_security_group" "sg_example" {
+  vpc_id = aws_vpc.vpc_example.id
+  name = "Allow SSH"
+  tags = {
+    Project = "terraform-training-01"
+    Name    = "Allow SSH"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "sg_ingress_rule_example" {
+  security_group_id = aws_security_group.sg_example.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port = 22
+  to_port = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "sg_egress_rule_example" {
+  security_group_id = aws_security_group.sg_example.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = -1
+}
+
+//To get "Host" and "DB" data in secret manager it is necessary to add a secret manager credentials in System Manager on aws (see instruction above) with key/value: Host/localhost and DB/db
 resource "aws_instance" "instance_example" {
   ami = "ami-0de716d6197524dd9"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet_example.id
+  vpc_security_group_ids = [ aws_security_group.sg_example.id ]
   tags = {
     Project = "terraform-training-01"
     Name = "instance-example-name"
   }
+  user_data = <<EOF
+#!/bin/bash
+DB_STRING="Server=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["Host"]};DB=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["DB"]}"
+echo $DB_STRING > test.txt
+EOF
 }
 
 resource "aws_internet_gateway" "igw_example" {
@@ -48,6 +87,18 @@ resource "aws_internet_gateway" "igw_example" {
   }
 }
 
+resource "aws_route_table" "route_table_example" {
+  vpc_id = aws_vpc.vpc_example.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_example.id
+  }
+  tags = {
+    Project = "terraform-training-01"
+    Name    = "route-table-example-name"
+  }
+}
+
 resource "aws_eip" "eip_example" {
   instance = aws_instance.instance_example.id
   tags = {
@@ -55,6 +106,11 @@ resource "aws_eip" "eip_example" {
     Name = "eip-example-name"
   }
   depends_on = [ aws_internet_gateway.igw_example ]
+}
+
+resource "aws_route_table_association" "route_table_association_example" {
+  subnet_id      = aws_subnet.subnet_example.id
+  route_table_id = aws_route_table.route_table_example.id
 }
 
 resource "aws_ssm_parameter" "ssm_parameter_example" {
