090c170665520ab8d8e40b84cdf9f5a615204b1a
Alexandre Clemente <aleclemente@gmail.com>
2025-08-12 18:24:39 -0300

    feat: add autoscaling policy and cloudwatch alarm

--- aws-provider-example/modules/cluster/main.tf
+++ aws-provider-example/modules/cluster/main.tf
@@ -38,6 +38,7 @@ EOF
 }
 
 resource "aws_autoscaling_group" "asg" {
+  name              = "${var.prefix}-asg"
   min_size          = 1
   desired_capacity  = 2
   max_size          = 3
@@ -47,5 +48,55 @@ resource "aws_autoscaling_group" "asg" {
   launch_template {
     id      = aws_launch_template.template.id
     version = "$Latest"
+  }
+}
+
+resource "aws_autoscaling_policy" "scale_out_policy" {
+  name                   = "${var.prefix}-scale-out"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = 1
+  cooldown               = 300
+}
+
+resource "aws_cloudwatch_metric_alarm" "scale_out_alarm" {
+  alarm_description   = "Monitor Average CPU Utilization"
+  alarm_name          = "${var.prefix}-scale-out-alarm"
+  namespace          = "AWS/EC2"
+  metric_name        = "CPUUtilization"
+  threshold          = "60"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "3"
+  period             = "30"
+  statistic          = "Average"
+  alarm_actions = [aws_autoscaling_policy.scale_out_policy.arn]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
+}
+
+resource "aws_autoscaling_policy" "scale_in_policy" {
+  name                   = "${var.prefix}-scale-in"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = -1
+  cooldown               = 300
+}
+
+resource "aws_cloudwatch_metric_alarm" "scale_in_alarm" {
+  alarm_description   = "Monitor Average CPU Utilization"
+  alarm_name          = "${var.prefix}-scale-in-alarm"
+  namespace          = "AWS/EC2"
+  metric_name        = "CPUUtilization"
+  threshold          = "20"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "3"
+  period             = "30"
+  statistic          = "Average"
+  alarm_actions = [aws_autoscaling_policy.scale_in_policy.arn]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
   }
 }
